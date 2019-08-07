2f66d7fe2d08a12c2face163d8768d5b6c2b505c
local <kmichel37@gmail.com>
2019-08-06 23:21:43 -0500

    Added Reveal on Scroll effects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
 import MobileMenu from "./js/modules/MobileMenu";
+import RevealOnScroll from "./js/modules/RevealOnScroll";
 
 var mobileMenu = new MobileMenu();
+
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
