615e575709ab8639e34af87d439e8e7f3c300793
local <kmichel37@gmail.com>
2019-08-09 16:20:13 -0500

    Created a Sticky header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import $ from 'jquery';
 import MobileMenu from "./js/modules/MobileMenu";
 import RevealOnScroll from "./js/modules/RevealOnScroll";
+import StickyHeader from "./js/modules/stickyHeader";
 
 var mobileMenu = new MobileMenu();
 
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+
+var stickyHeader = new StickyHeader();
