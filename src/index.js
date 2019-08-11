80012eb876795bd9baa73bb52a7ab76bf5311429
local <kmichel37@gmail.com>
2019-08-10 19:53:00 -0500

    Completed the Modal function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import MobileMenu from "./js/modules/MobileMenu";
 import RevealOnScroll from "./js/modules/RevealOnScroll";
 import StickyHeader from "./js/modules/stickyHeader";
+import Modal from "./js/modules/Modal";
 
 var mobileMenu = new MobileMenu();
 
@@ -9,3 +10,4 @@ new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
