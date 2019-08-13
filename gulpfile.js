cd4e103cd043a1eb042f84eae24b9e2594806996
local <kmichel37@gmail.com>
2019-08-13 15:17:46 -0500

    fixed gulp-imagemin setup

--- gulpfile.js
+++ gulpfile.js
@@ -171,11 +171,17 @@ function gulpClean() {
 
 function optimizeImages() {
     return src(imageFiles)
-    .pipe(imagemin({
-        progressive: true, // *helps with jpg files
-        interlaced: true, // *helps with png files
-        multipass: true // *helps with svg files
-    }))
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
     .pipe(dest('prod/assets/images'));
 }
 
